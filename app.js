b34da63786aa1db783909ad5be1593f503458583
HafenaajeA <almandocode@gmail.com>
2024-05-14 08:42:57 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const counterEl = document.querySelectorAll(".counter");
+
+counterEl.forEach((counterElement) => {
+  counterElement.innerText = "0";
+  incrementCounter();
+  function incrementCounter() {
+    let currentNum = +counterElement.innerText;
+    const dataCeil = counterElement.getAttribute("data-ceil");
+    const increment = dataCeil / 15;
+    currentNum = Math.ceil(currentNum + increment);
+
+    if (currentNum < dataCeil) {
+      counterElement.innerText = currentNum;
+      setTimeout(incrementCounter, 50);
+    } else {
+      counterElement.innerText = dataCeil;
+    }
+  }
+});
